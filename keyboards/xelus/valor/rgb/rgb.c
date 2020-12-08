284aba66eccc0d469066d297d9d7574e16008a3e
Xelus22 <preyas22@gmail.com>
2020-12-08 18:52:14 +1100

    move dma channels

--- keyboards/xelus/valor/rgb/rgb.c
+++ keyboards/xelus/valor/rgb/rgb.c
@@ -22,5 +22,5 @@
 //void matrix_io_delay(void) { __asm__ volatile("nop\nnop\nnop\n"); }
 
 void board_init(void) {
-  SYSCFG->CFGR1 &= ~(SYSCFG_CFGR1_I2C1_DMA_RMP);
+  SYSCFG->CFGR1 |= SYSCFG_CFGR1_I2C1_DMA_RMP;
 }
