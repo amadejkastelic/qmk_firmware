3cc32e8244b1538a21f0ef24372bcb50c3d03d32
Xelus22 <preyas22@gmail.com>
2021-01-16 17:41:14 +1100

    mendez

--- keyboards/preonic/keymaps/via/gen.py
+++ keyboards/preonic/keymaps/via/gen.py
@@ -4,11 +4,13 @@
 import csv, sys, re
 
 def main(args):
-    for x in range(0, 5):
-        for y in range(0, 12):
-            print("ROW" + str(x) + "COL" + str(y)+",", end = '')
-
-        print("")
+    for y in range(0, 12):
+        for x in range(0, 5):
+            print("case " +  "ROW" + str(x) + "COL" + str(y) + ":")
+            print('    SEND_STRING("");')
+            print("    SEND_STRING(SS_TAP(X_ENTER));")
+            print("    return false;")
+            print("    break;")
 
 if __name__ == "__main__":
     main(sys.argv)
