14b54b15579c93f5f083b57606d7cc98424d18a3
Xelus22 <preyas22@gmail.com>
2021-06-17 20:47:01 +1000

    pachi prod init

--- keyboards/xelus/pachi/rgb/rules.mk
+++ keyboards/xelus/pachi/rgb/rules.mk
@@ -8,7 +8,7 @@ BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
+COMMAND_ENABLE = yes        # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
@@ -24,6 +24,7 @@ EEPROM_DRIVER = i2c
 
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = custom
+RGB_MATRIX_CUSTOM_KB = yes
 
 COMMON_VPATH += $(DRIVER_PATH)/issi
 SRC += drivers/issi/is31fl3741.c
