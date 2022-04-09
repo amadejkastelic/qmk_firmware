cc7c0544c835d558f6e7bc6dfbabfb69b812461c
xelus22 <preyas22@gmail.com>
2022-04-09 15:32:45 +1000

    finish keymaps

--- keyboards/xelus/jmun_bear65/rules.mk
+++ keyboards/xelus/jmun_bear65/rules.mk
@@ -10,8 +10,8 @@ BOOTLOADER = atmel-dfu
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
-COMMAND_ENABLE = yes        # Commands for debug and configuration
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
@@ -19,5 +19,9 @@ AUDIO_ENABLE = no           # Audio output
 
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
+
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no 
+MAGIC_ENABLE = no
 
 LTO_ENABLE = yes
