71cf2312d1f9f51c8b87c9a88b912e614a384636
zykrah <zykrahgaming@gmail.com>
2022-05-18 18:34:44 +1000

    Added VIAL keymap for Q2 (ANSI KNOB)

--- keyboards/keychron/q2/q2_ansi_stm32l432_ec11/q2_ansi_stm32l432_ec11.c
+++ keyboards/keychron/q2/q2_ansi_stm32l432_ec11/q2_ansi_stm32l432_ec11.c
@@ -129,7 +129,7 @@ led_config_t g_led_config = {
 
 #endif
 
-#ifdef ENCODER_ENABLE
+#if !defined(VIAL_ENCODERS_ENABLE) && defined(ENCODER_ENABLE)
 
 bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (!encoder_update_user(index, clockwise)) { return false; }
