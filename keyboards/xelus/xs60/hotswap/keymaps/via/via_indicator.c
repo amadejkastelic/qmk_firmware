621081fc8373fd4a1283ce740faa5e02e9c1826b
Xelus22 <preyas22@gmail.com>
2023-08-12 20:46:13 +1000

    fix up

--- keyboards/xelus/xs60/hotswap/keymaps/via/via_indicator.c
+++ keyboards/xelus/xs60/hotswap/keymaps/via/via_indicator.c
@@ -256,7 +256,8 @@ void _get_color(HSV *color, uint8_t *data) {
 
 // Set the indicators with RGB Matrix subsystem
 bool rgb_matrix_indicators_user() {
-    int8_t highest_priority = 5;
+    int8_t highest_priority = INT8_MAX;
+    bool bLedSet = false;
     // 0 has the highest priority
     // 5 has the lowest
 
@@ -284,39 +285,48 @@ bool rgb_matrix_indicators_user() {
     }
 
     // caps lock cyan
-    if (highest_priority == g_config.caps_lock_key) {
+    if (host_keyboard_led_state().caps_lock && highest_priority == g_config.caps_lock_key) {
         RGB rgb_caps = hsv_to_rgb((HSV){.h = g_config.caps_lock_indicator.h,
                                         .s = g_config.caps_lock_indicator.s,
                                         .v = g_config.caps_lock_indicator.v});
         rgb_matrix_set_color(0, rgb_caps.r, rgb_caps.g, rgb_caps.b);
+        bLedSet = true;
     }
 
     // num lock cyan
-    if (highest_priority == g_config.num_lock_key) {
+    if (host_keyboard_led_state().num_lock && highest_priority == g_config.num_lock_key && !bLedSet) {
         RGB rgb_num = hsv_to_rgb((HSV){.h = g_config.num_lock_indicator.h,
                                        .s = g_config.num_lock_indicator.s,
                                        .v = g_config.num_lock_indicator.v});
         rgb_matrix_set_color(0, rgb_num.r, rgb_num.g, rgb_num.b);
+        bLedSet = true;
     }
 
     // scroll lock cyan
-    if (highest_priority == g_config.scroll_lock_key) {
-        RGB rgb_scroll = hsv_to_rgb((HSV){.h = g_config.scroll_lock_indicator.h, .s = g_config.scroll_lock_indicator.s, .v = g_config.scroll_lock_indicator.v});
+    if (host_keyboard_led_state().scroll_lock && highest_priority == g_config.scroll_lock_key && !bLedSet) {
+        RGB rgb_scroll = hsv_to_rgb((HSV){.h = g_config.scroll_lock_indicator.h,
+                                          .s = g_config.scroll_lock_indicator.s,
+                                          .v = g_config.scroll_lock_indicator.v});
         rgb_matrix_set_color(0, rgb_scroll.r, rgb_scroll.g, rgb_scroll.b);
+        bLedSet = true;
     }
 
     // layer state
-    if (highest_priority == g_config.enable_layer_indicator) {
-        RGB rgb_layer = hsv_to_rgb((HSV){.h = g_config.layer_indicator.h, .s = g_config.layer_indicator.s, .v = g_config.layer_indicator.v});
+    if (highest_priority == g_config.enable_layer_indicator && !bLedSet) {
+        RGB rgb_layer = hsv_to_rgb((HSV){.h = g_config.layer_indicator.h,
+                                         .s = g_config.layer_indicator.s,
+                                         .v = g_config.layer_indicator.v});
         switch (get_highest_layer(layer_state)) {
             case 0:
                 break;
             case 1:
                 rgb_matrix_set_color(0, rgb_layer.r, rgb_layer.g, rgb_layer.b);
+                bLedSet = true;
                 break;
             default:
                 // white
                 rgb_matrix_set_color(0, 128, 128, 128);
+                bLedSet = true;
                 break;
         }
     }
