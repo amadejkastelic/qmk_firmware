72ec42658c3931e4f6650e5d3e24912f56b3df38
Xelus22 <preyas22@gmail.com>
2023-10-28 14:44:25 +1100

    initial kangaroo with infinite eeprom and 128 macros

---
+++ keyboards/xelus/kangaroo/rev2_1/config.h
@@ -0,0 +1,34 @@
+/* Copyright 2020 Harrison Chan (Xelus)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define DYNAMIC_KEYMAP_MACRO_COUNT 128
+
+#define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 65535
+
+#define EEPROM_I2C_CAT24C512
+
+// I2C setup
+#define I2C1_SCL_PIN        B8
+#define I2C1_SDA_PIN        B9
+#define I2C1_SCL_PAL_MODE   4
+#define I2C1_SDA_PAL_MODE   4
+#define I2C1_TIMINGR_PRESC  0U
+#define I2C1_TIMINGR_SCLDEL 7U
+#define I2C1_TIMINGR_SDADEL 0U
+#define I2C1_TIMINGR_SCLH   45U
+#define I2C1_TIMINGR_SCLL   149U
